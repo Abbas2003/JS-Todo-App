60b9f112c9142639f02c8d168aca55f495c0cffa
Abbas2003 <abbas.mohammad805@gmail.com>
2024-05-09 21:25:04 +0500

    complete

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 var userInput = document.getElementById("userInput")
-console.log(userInput.value)
+var main = document.getElementById("main")
+// console.log(userInput.value)
 
-function getUserValue(){
-    var text = document.createTextNode(userInput.value)
-    var node = document.createElement("li")
-    var editBtn = document.createElement("button")
-    editBtn.textContent = "Edit"
-    var delBtn = document.createElement("button")
-    delBtn.textContent = "Delete"
-    // delBtn.style={ backgroundColor: "red"}
-    
-    node.appendChild(text)
-    document.getElementById("main").appendChild(node).appendChild(delBtn)
-    text.value = " "
-    console.log(text)
+function addItem() {
+    if (userInput.value !== "") {
+        var node = document.createElement("P")
+        var text = document.createTextNode(userInput.value)
+        node.appendChild(text)
+
+        var editBtn = document.createElement("BUTTON")
+        var editText = document.createTextNode("Edit")
+        editBtn.setAttribute('onclick', 'editItem(this)')
+        editBtn.setAttribute('class', 'editButton')
+        editBtn.appendChild(editText)
+
+        var delBtn = document.createElement("BUTTON")
+        var delText = document.createTextNode("Delete")
+        delBtn.setAttribute('onclick', 'deleteItem(this)')
+        delBtn.setAttribute('class', 'deleteBtn')
+        delBtn.appendChild(delText)
+
+        node.appendChild(editBtn)
+        node.appendChild(delBtn)
+
+        main.appendChild(node)
+        //    console.log(node)
+    } else {
+        alert("Invalid Input")
+    }
+
+    userInput.value = ""
 }
 
-// function clear(){
-//     userInput.value = ""
-// }
+function deleteItem(btn) {
+    btn.parentNode.remove()
+    
+    console.log(btn.parentNode)
+}
+
+function editItem(btn) {
+    var editVal = prompt("Edit your todo")
+    btn.parentNode.childNodes[0].nodeValue = editVal
+    
+    console.log(btn.parentNode.childNodes[0])
+}
